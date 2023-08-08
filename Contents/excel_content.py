e9ff60f2f821ba82dc11ad44f0b64163e943b83b
ByteBard <139046842+bytecanvas@users.noreply.github.com>
2023-08-08 14:37:57 +0545

    Create excel_content.py

---
+++ Contents/excel_content.py
@@ -0,0 +1,9 @@
+# excel_content.py
+
+def display_excel_content():
+    print("📊 Excel:")
+    print("1a. Excel Basic")
+    print("1b. Data Visualization")
+    print("1c. Data Analytics")
+    print("1d. Power Query")
+    print("1e. Project")
