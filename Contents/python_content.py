d68c28e9028b4e2387c324885581718490179eee
ByteBard <139046842+bytecanvas@users.noreply.github.com>
2023-08-08 14:38:39 +0545

    Create python_content.py

---
+++ Contents/python_content.py
@@ -0,0 +1,8 @@
+# python_content.py
+
+def display_python_content():
+    print("🐍 Python:")
+    print("3a. Python Basics (100 Days)")
+    print("3b. Data Analysis with Python")
+    print("3c. Data Visualization with Python")
+    print("3d. Python Project")
