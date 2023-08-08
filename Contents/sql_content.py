a97b625bedb33f0c6928848c5304c344fe538e55
ByteBard <139046842+bytecanvas@users.noreply.github.com>
2023-08-08 14:38:14 +0545

    Create sql_content.py

---
+++ Contents/sql_content.py
@@ -0,0 +1,8 @@
+# sql_content.py
+
+def display_sql_content():
+    print("🔍 SQL:")
+    print("2a. SQL Basics")
+    print("2b. SQL Data Analysis")
+    print("2c. SQL and Power BI")
+    print("2d. Project")
